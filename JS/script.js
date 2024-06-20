f0131d0cc4cd3861a03ec1b78ffe76c1a68f8fbe
Rachma-Nurhaliza-Parindra <rachmanurhalizastudent@gmail.com>
2024-06-20 23:03:44 +0700

    Final Project

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,14 @@
 let lastConversion = "CtoF"; // Initialize to track the last conversion type
 
+function isNumberKey(evt) {
+    const charCode = evt.which ? evt.which : evt.keyCode;
+    if (charCode !== 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+        evt.preventDefault();
+        return false;
+    }
+    return true;
+}
+
 function convertToFahrenheit() {
     const celsiusInput = document.getElementById("celsius").value;
     if (celsiusInput === "") {
